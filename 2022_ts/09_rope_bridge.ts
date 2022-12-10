9ef5d0c5bdf54433ab312117f1f39e0d79464e72
Leo Antoli <leoantol@amazon.com>
2022-12-10 09:47:11 +0100

    day 09

---
+++ 2022_ts/09_rope_bridge.ts
@@ -0,0 +1,47 @@
+import { readFileSync, stat } from 'fs';
+
+const inputContent = readFileSync("inputs/09_input.txt", 'utf-8');
+const lines = inputContent.split('\n');
+lines.pop(); // Remove last empty line
+
+const input: [string, number][] = lines.map(line => line.split(" ")).map(([dir, num]) => [dir, Number(num)]);
+
+type Coord = {y: number, x: number};
+
+function moveTail(tail: Coord, head: Coord) : Coord {
+    if (head.y == tail.y && Math.abs(head.x - tail.x) == 2) {
+        tail = {...tail, x: tail.x + Math.sign(head.x - tail.x)};
+    } else if (head.x == tail.x && Math.abs(head.y - tail.y) == 2) {
+        tail = {...tail, y: tail.y + Math.sign(head.y - tail.y)};
+    } else if (Math.abs(head.x - tail.x) == 2 || Math.abs(head.y - tail.y) == 2 ) {
+        tail = { y: tail.y + Math.sign(head.y - tail.y), x: tail.x + Math.sign(head.x - tail.x) };
+    }
+    return tail;
+}
+
+function run(knotCount: number) {
+    const moveHead = (yinc: number, xinc: number) => knots[0] = { y: knots[0].y + yinc, x: knots[0].x + xinc };
+    const knots: Coord[] = [];
+    for (let i = 0; i < knotCount; i++) knots.push({ y: 0, x: 0});
+    let tailHist = [ knots[knotCount - 1] ];
+    
+    for (let [dir, num] of input) for (let i = 0; i < num; i++) {
+        switch(dir) {
+            case "R": moveHead(0, 1); break;
+            case "L": moveHead(0, -1); break;
+            case "D": moveHead(1, 0); break;
+            case "U": moveHead(-1, 0); break;
+            default: throw new Error("unknown");
+        }
+        for (let i = 1; i < knotCount; i++) knots[i] = moveTail(knots[i], knots[i-1]);
+
+        const tail = knots[knotCount-1];
+        if (!tailHist.some(elm => elm.x === tail.x && elm.y === tail.y)) {
+            tailHist.push(tail);
+        }
+    }
+    console.log(tailHist.length);
+};
+
+run(2); // 6030 
+run(10); // 2545
