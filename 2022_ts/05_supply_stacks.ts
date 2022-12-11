c2c4c7214455c25e15e1ef617ea10bce62c56356
Leo Antoli <leoantol@amazon.com>
2022-12-11 10:04:50 +0100

    day 05 extract method

--- 2022_ts/05_supply_stacks.ts
+++ 2022_ts/05_supply_stacks.ts
@@ -4,8 +4,9 @@ const inputContent = readFileSync("inputs/05_input.txt", 'utf-8');
 const lines = inputContent.split('\n');
 lines.pop(); // Remove last empty line
 
-type Move = { move: number, from: number, to:number };
-type Stacks = string[][];
+type Move = { times: number, from: number, to:number };
+type Stack = string[];
+type Stacks = Stack[];
 
 const originalStacks: Stacks = [
     [ "N", "D", "M", "Q", "B", "P", "Z"],
@@ -20,32 +21,29 @@ const originalStacks: Stacks = [
 ];
 
 const moves: Move[] = lines.filter(line => line.startsWith("move")).map(line => line.replaceAll(/move |from |to /g, "")).
-    map(numbers => numbers.split(" ")).map(n => ({ move: Number(n[0]), from: Number(n[1]) - 1, to: Number(n[2]) - 1 }));
+    map(numbers => numbers.split(" ")).map(n => ({ times: Number(n[0]), from: Number(n[1]) - 1, to: Number(n[2]) - 1 }));
 
-const moveOne1 = (stacks: Stacks, move: Move) => { 
-    for (let i = 0; i < move.move; i++) {
-        stacks[move.to].push(stacks[move.from].pop()!) 
-    }
+function moveOne1(stacks: Stacks, { times, from, to }: Move) : void { 
+    moveCrates(stacks[from], stacks[to], times);
 };
 
-const moveOne2 = (stacks: Stacks, move: Move) => { 
-    const temp: string[] = [];
-    for (let i = 0; i < move.move; i++) {
-        temp.push(stacks[move.from].pop()!) 
-    }
-    for (let i = 0; i < move.move; i++) {
-        stacks[move.to].push(temp.pop()!) 
-    }
+function moveOne2(stacks: Stacks, { times, from, to }: Move) { 
+    const temp: Stack = [];
+    moveCrates(stacks[from], temp, times);
+    moveCrates(temp, stacks[to], times);
 };
+
+function moveCrates(stackFrom: Stack, stackTo: Stack, times: number) : void {
+    for (let i = 0; i < times; i++) {
+        stackTo.push(stackFrom.pop()!) 
+    }
+}
 
 function run(moveOne: (stacks: Stacks, move: Move) => void) {
     const clone = (input: string[]) => input.map(elm => elm);
     const stacks = originalStacks.map(elm => clone(elm));
-    for (let move of moves) {
-        moveOne(stacks, move);
-    }
-    const result = stacks.map(stack => stack.pop()!).join(""); 
-    console.debug(result);
+    moves.forEach(move => moveOne(stacks, move)); 
+    console.debug(stacks.map(stack => stack.pop()!).join(""));
 };
 
 run(moveOne1); // QGTHFZBHV
