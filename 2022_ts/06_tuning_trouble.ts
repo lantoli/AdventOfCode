fa9f0b08c8830d86e7b8282600340a9fc266fd9b
Leo Antoli <leoantol@amazon.com>
2022-12-11 09:06:23 +0100

    day 06 remove unused vars

--- 2022_ts/06_tuning_trouble.ts
+++ 2022_ts/06_tuning_trouble.ts
@@ -1,8 +1,6 @@
 import { readFileSync } from 'fs';
 
 const inputContent = readFileSync("inputs/06_input.txt", 'utf-8');
-const lines = inputContent.split('\n');
-lines.pop(); // Remove last empty line
 
 function position(data: String, len: number) : number {
     for (let i = len; i <= data.length; i++) {
