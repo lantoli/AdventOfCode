64087aea28c5c047220c93f9c87c111be77a228a
Leo Antoli <leoantol@amazon.com>
2022-12-11 09:16:25 +0100

    day 01 unite parts

--- 2022_ts/01_calorie_counting.ts
+++ 2022_ts/01_calorie_counting.ts
@@ -3,32 +3,21 @@ import { readFileSync } from 'fs';
 const inputContent = readFileSync("inputs/01_input.txt", 'utf-8');
 const lines = inputContent.split('\n');
 
-function part1() {
-    let total = 0, max = 0;
-    for (let line of lines) {
-        const val = +line;
-        total = val === 0 ? 0 : total + val;
-        max = Math.max(max, total);
-    }
-
-    console.debug(max); // 67027 
-};
-
-function part2() {
+function run(count: number) {
     let total = 0;
     let list: number[] = []; // We could use a Heap but problem is small
 
     for (let line of lines) {
-        const val = +line;
+        const val = Number(line);
+        total += val;
         if (val == 0) {
             list.push(total);
             total = 0;
-        } else {
-            total += val;
         }
     }
-    const s = list.sort((a, b) => a - b);
-    console.debug(s.pop()! + s.pop()! + s.pop()!); // 197291
+    const top = list.sort((a, b) => b - a).slice(0, count);
+    console.debug(top.reduce((a, b) => a + b)); 
 };
 
-part2();
+run(1); // 67027
+run(3); // 197291
