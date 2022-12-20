b9a3bd61a8689076da09494ed64509d4a1eb9bb1
Leo Antoli <leoantol@amazon.com>
2022-12-20 17:48:11 +0100

    day 16 part 1

--- 2022_ts/16_proboscidea_volcanium.ts
+++ 2022_ts/16_proboscidea_volcanium.ts
@@ -4,7 +4,7 @@ const inputContent = readFileSync("inputs/16_input.txt", 'utf-8');
 const sampleContent = readFileSync("inputs/16_sample.txt", 'utf-8');
 
 type Valve = { name: string; rate: number; children: string[]; };
-type State = { current: Valve, open: Set<string>, pressure: number, minute: number };
+type State = { current: Valve, open: Set<string>, visited: Set<string>, pressure: number, minute: number };
 
 function run(content: string) {
     const lines = content.split('\n');
@@ -16,7 +16,7 @@ function run(content: string) {
     });
     
     const first = valves.find(v => v.name === "AA")!;
-    const states: State[] = [{ current: first, open: new Set([first.name]), pressure: 0, minute: 1 }];
+    const states: State[] = [{ current: first, open: new Set(), visited: new Set([first.name]), pressure: 0, minute: 1 }];
 
     const minutes = 30;
 
@@ -24,7 +24,9 @@ function run(content: string) {
 
     let ret = 0;
     while (states.length > 0) {
-        const {current, open, pressure, minute} = states.shift()!;
+        const {current, open, visited, pressure, minute} = states.pop()!;
+        //const toOpen = valves.filter(valve => !open.has(valve.name) && valve.rate > 0).map(v => v.rate).reduce((a, b) => a + b, 0);
+        // if (minute + toOpen > minutes) continue;
         if (open.size === valves.length) continue;
         for (let child of current.children) {
             const newValve = valves.find(v => v.name === child)!;
@@ -32,23 +34,29 @@ function run(content: string) {
                 const newMinute = minute + 2;
                 if (newMinute < minutes) {
                     const newPressure = newValve.rate * (minutes - newMinute + 1);
-                    const newState = { current: newValve, open: new Set([child, ...open]), pressure: pressure + newPressure, minute: newMinute };
+                    const newState: State = { current: newValve, open: new Set([child, ...open]), visited: new Set([child]), pressure: pressure + newPressure, minute: newMinute };
                     states.push(newState);
-                    ret = Math.max(ret, newState.pressure);
+
+                    if (newState.pressure > ret) { ret = newState.pressure; console.debug("PRESSURE", ret); }
+
+                    //ret = Math.max(ret, newState.pressure);
                     // console.debug(minute, pressure, newPressure, newState);    
                     if (newMinute > maxMinute) { maxMinute = newMinute; console.debug("max1", maxMinute); }
                 }    
             }
 
-            const newMinute = minute + 1;
-            if (newMinute < minutes) {
-                if (newMinute > maxMinute) { maxMinute = newMinute; console.debug("max2", maxMinute); }
-                const newState = { current: newValve, open: new Set([...open]), pressure, minute: newMinute };
-                states.push(newState);
-        }
+            if (!visited.has(child)) {
+                const newMinute = minute + 1;
+                if (newMinute < minutes) {
+                    if (newMinute > maxMinute) { maxMinute = newMinute; console.debug("max2", maxMinute); }
+                    const newState = { current: newValve, open, visited: new Set([child, ...visited]), pressure, minute: newMinute };
+                    states.push(newState);
+                }
+            }
         }
     }
     console.debug(ret);
 };
 
 run(sampleContent); // 1651 (sample)
+run(inputContent); // 2029
