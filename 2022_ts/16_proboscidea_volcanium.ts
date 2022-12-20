de0289e6d2434820e047c9adb274159c0de005b6
Leo Antoli <leoantol@amazon.com>
2022-12-20 23:13:13 +0100

    day 16 part 2 but takes 7m

--- 2022_ts/16_proboscidea_volcanium.ts
+++ 2022_ts/16_proboscidea_volcanium.ts
@@ -3,62 +3,100 @@ import { readFileSync } from 'fs';
 const inputContent = readFileSync("inputs/16_input.txt", 'utf-8');
 const sampleContent = readFileSync("inputs/16_sample.txt", 'utf-8');
 
-type Valve = { name: string; rate: number; children: string[]; };
+type Valve = { name: string; rate: number; childNames: string[]; children: Valve[] };
 type Player = { current: Valve, visited: Set<string> };
-type State = { me: Player, open: Set<string>, pressure: number, minute: number, remainRate: number };
+type State = { me: Player, el: Player, open: Set<string>, pressure: number, minute: number, remainRate: number };
 
-function run(content: string, minutes: number, withHelp: boolean) {
+function run(content: string, minutes: number, withElephant: boolean) {
     const lines = content.split('\n');
     lines.pop(); // Remove last empty line
 
     const valves: Valve[] = lines.map(line => {
-        const [_full, name, rate, children] = line.match(/Valve (.*) has flow rate=(.*); tunnels? leads? to valves? (.*)/)!;
-        return { name, rate: Number(rate), children: children.split(", ") }
+        const [_full, name, rate, childName] = line.match(/Valve (.*) has flow rate=(.*); tunnels? leads? to valves? (.*)/)!;
+        return { name, rate: Number(rate), childNames: childName.split(", "), children: [] };
     });
+    for (let valve of valves) {
+        valve.children = valve.childNames.map(name => valves.find(v => v.name === name)!);
+    }
     
     const remainRate = valves.map(valve => valve.rate).reduce((a, b) => a + b);
-    const first = valves.find(v => v.name === "AA")!;
-    const me: Player = { current: first, visited: new Set([first.name]) };
-    const states: State[] = [{ me, open: new Set(), pressure: 0, minute: 1, remainRate }];
-
+    const firstValve = valves.find(v => v.name === "AA")!;
+    const firstPlayer: Player = { current: firstValve, visited: new Set([firstValve.name]) };
+    const states: State[] = [{ me: firstPlayer, el: firstPlayer, open: new Set(), pressure: 0, minute: 1, remainRate }];
 
     let maxMinute = 0;
 
     let ret = 0;
     while (states.length > 0) {
-        const {me, open, pressure, minute, remainRate} = states.pop()!;
+        const {me, el, open, pressure, minute, remainRate} = states.pop()!;
         const newMinute = minute + 1;
         const remainingMinutes = minutes - newMinute + 1;
         const optimistic = remainRate * remainingMinutes;
         if (optimistic <= 0 || ret > pressure + optimistic || open.size === valves.length) continue;
-        
-        if (me.current.rate > 0 && !open.has(me.current.name)) {
+
+        const newMes: Player[] = me.current.children.filter(child => !me.visited.has(child.name)).map(child => 
+            ({ current: child, visited: new Set([child.name, ...me.visited])}));
+
+        const newEls: Player[] = el.current.children.filter(child => !el.visited.has(child.name)).map(child => 
+            ({ current: child, visited: new Set([child.name, ...el.visited])}));
+
+        const openMe = me.current.rate > 0 && !open.has(me.current.name);
+        const openEl = el.current.rate > 0 && !open.has(el.current.name);
+
+        if (openMe) {
             const newPressure = me.current.rate * remainingMinutes;
-            const newMe = { current: me.current, visited: new Set([me.current.name]) };
-            const newState: State = { me: newMe, open: new Set([me.current.name, ...open]), 
+            const newMe: Player = { current: me.current, visited: new Set([me.current.name]) };
+            let newState: State = { me: newMe, el, open: new Set([me.current.name, ...open]), 
                 pressure: pressure + newPressure, minute: newMinute, remainRate: remainRate - me.current.rate };
-            states.push(newState);
+            ret = Math.max(ret, newState.pressure);
 
-            if (newState.pressure > ret) { ret = newState.pressure; console.debug("PRESSURE", ret); }
+            if (withElephant && openEl && me.current !== el.current) {
+                const newPressure = el.current.rate * remainingMinutes;
+                const newEl: Player = { current: el.current, visited: new Set([el.current.name]) };
+                states.push({...newState, el: newEl, open: new Set([el.current.name, ...newState.open]), 
+                    pressure: newState.pressure + newPressure, remainRate: newState.remainRate - el.current.rate });
+                ret = Math.max(ret, newState.pressure + newPressure);
+            }
 
-            //ret = Math.max(ret, newState.pressure);
-            // console.debug(minute, pressure, newPressure, newState);    
-            if (minute+1 > maxMinute) { maxMinute = minute+1; console.debug("max1", maxMinute); }
+            if (!withElephant || newEls.length == 0) {
+                states.push(newState);
+            } else for (let newEl of newEls) {
+                states.push({...newState, el: newEl });
+            }
         }
 
-        for (let child of me.current.children) if (!me.visited.has(child)) {
-            const newValve = valves.find(v => v.name === child)!;
-            const newMe = { current: newValve, visited: new Set([child, ...me.visited]) };
-            const newState = { me: newMe, current: newValve, open, pressure, minute: newMinute, remainRate };
-            states.push(newState);
+        if (withElephant && openEl) {
+            const newPressure = el.current.rate * remainingMinutes;
+            const newEl: Player = { current: el.current, visited: new Set([el.current.name]) };
+            let newState: State = { me, el: newEl, open: new Set([el.current.name, ...open]), 
+                pressure: pressure + newPressure, minute: newMinute, remainRate: remainRate - el.current.rate };
+            ret = Math.max(ret, newState.pressure);
+
+            if (openMe && me.current !== el.current) {
+                const newPressure = me.current.rate * remainingMinutes;
+                const newMe: Player = { current: me.current, visited: new Set([me.current.name]) };
+                states.push({...newState, me: newMe, open: new Set([me.current.name, ...newState.open]), 
+                    pressure: newState.pressure + newPressure, remainRate: newState.remainRate - me.current.rate });
+                ret = Math.max(ret, newState.pressure + newPressure);
+                }
+            
+            if (newMes.length == 0) {
+                states.push(newState);
+            } else for (let newMe of newMes) {
+                states.push({...newState, me: newMe });
+            }
         }
+
+        if (withElephant) for (let newMe of newMes) for (let newEl of newEls) {
+            states.push({ me: newMe, el: newEl, open, pressure, minute: newMinute, remainRate });
+        }   
+        if (!withElephant || newEls.length == 0) for (let newMe of newMes) states.push({ me: newMe, el, open, pressure, minute: newMinute, remainRate });
+        if (newMes.length == 0) for (let newEl of newEls) states.push({ me, el: newEl, open, pressure, minute: newMinute, remainRate });
     }
     console.debug(ret);
 };
 
-//run(sampleContent, 30, false); // 1651 (sample)
-
-run(sampleContent, 26, true); // 1707 (sample) TEMPORAL 1327
-
-//run(inputContent, 30, false); // 2029
-//run(inputContent, 26, true); // ...
+run(sampleContent, 30, false); // 1651 (sample)
+run(sampleContent, 26, true); // 1707 (sample)
+run(inputContent, 30, false); // 2029
+run(inputContent, 26, true); // 2723
