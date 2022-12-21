d95a6052427cf51a7a8fd51680910b0cf527b594
Leo Antoli <leoantol@amazon.com>
2022-12-21 21:02:03 +0100

    day 17 part 1

---
+++ 2022_ts/17_pyroclastic_flow.ts
@@ -0,0 +1,107 @@
+import { readFileSync } from 'fs'
+
+const inputContent = readFileSync("inputs/17_input.txt", 'utf-8')
+const sampleContent = readFileSync("inputs/17_sample.txt", 'utf-8')
+
+const shapes = [
+    [ [true, true, true, true] ],
+    [
+        [false, true, false],
+        [true, true, true],
+        [false, true, false],
+    ],
+    [
+        [false, false, true],
+        [false, false, true],
+        [true, true, true],
+    ], [
+        [true],
+        [true],
+        [true],
+        [true],
+    ], [
+        [true, true],
+        [true, true]
+    ]
+]
+
+function display(board: boolean[][]) {
+    console.debug([...board].reverse().map(row => row.map(cell => cell ? '#' : '.').join('')).join('\n'))
+}
+
+function allowed(board: boolean[][], shape: boolean[][], ypos: number, xpos: number) : boolean {
+    for (let yshape = 0; yshape < shape.length; yshape++) for (let xshape = 0; xshape < shape[0].length; xshape++) {
+        const y = ypos - yshape
+        const x = xpos + xshape
+        if (shape[yshape][xshape] && board[y][x]) return false
+    }
+    return true;
+}
+
+const highestRock = (board: boolean[][]): number => {
+    for (let y = board.length - 1; y >= 0; y--) {
+        if (board[y].some(cell => cell)) {
+            return y
+        }
+    }
+    throw new Error("shouldnt happen")
+}
+
+function run(content: string, rocks: number) {
+    const lines = content.split('\n')
+    lines.pop() // Remove last empty line
+    const moves = [...lines[0]]
+
+    const width = 7
+    const board: boolean[][] = [new Array(width).fill(true)]
+    let moveIndex = -1
+
+    for (let count = 0, shapeIndex = 0; count < rocks; count++, shapeIndex = (shapeIndex + 1) % shapes.length) {
+        const shape  = shapes[shapeIndex]
+
+        let ypos = highestRock(board) + 3 + shape.length
+        // console.debug("ypos", ypos)
+        let xpos = 2
+        while (board.length <= ypos) board.push(new Array(width).fill(false))
+        //for (let y = 0; y < shape.length; y++) for (let x = 0; x < shape[0].length; x++) {
+        //    board[ypos + y][x + xpos] = shape[y][x]
+        //}
+        //console.debug("before")
+        //display(board)
+
+        while (true) {
+            moveIndex = (moveIndex + 1) % moves.length
+            const move = moves[moveIndex]
+
+            if (move === ">") {
+                if (xpos + shape[0].length < width) {
+                    if (allowed(board, shape, ypos, xpos+1)) xpos++;
+                }
+            } else { // <
+                if (xpos > 0) {
+                    if (allowed(board, shape, ypos, xpos-1)) xpos--;
+                }
+            }
+    
+            if (!allowed(board, shape, ypos, xpos)) break;
+
+            if (allowed(board, shape, ypos - 1, xpos)) {
+                ypos--
+            } else {
+                break
+            }
+        }
+
+        for (let y = 0; y < shape.length; y++) for (let x = 0; x < shape[0].length; x++) {
+            if (shape[y][x]) board[ypos - y][x + xpos] = true
+        }
+
+        // console.debug("count", count)
+        // display(board)
+}
+    
+    console.debug(highestRock(board))
+}
+
+run(sampleContent, 2022) // 3068 (sample)
+run(inputContent, 2022) // 3215
