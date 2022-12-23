936597e405faca2c0e800a0f93499ad4025a0ac1
Leo Antoli <leoantol@amazon.com>
2022-12-23 08:49:16 +0100

    day 18 part 1 clean

--- 2022_ts/18_boiling_boulders.ts
+++ 2022_ts/18_boiling_boulders.ts
@@ -1,4 +1,3 @@
-import { debug } from 'console';
 import { readFileSync } from 'fs';
 
 const inputContent = readFileSync("inputs/18_input.txt", 'utf-8');
@@ -6,7 +5,18 @@ const sampleContent = readFileSync("inputs/18_sample.txt", 'utf-8');
 
 const incs = [ [1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1] ]
 
-function run(content: string) {
+function fromCube(max: number, cube: number) : number[] {
+    const x = Math.floor(cube / (max * max))
+    const y = Math.floor((cube - x * max * max) / max)
+    const z = cube - x * max * max - y * max
+    return [x, y, z]
+}
+
+function toCube(max: number, [x, y, z]: [number, number, number]) : number {
+    return x * max * max + y * max + z
+}
+
+function run1(content: string) {
     const lines = content.split('\n');
     lines.pop(); // Remove last empty line
 
@@ -16,19 +26,17 @@ function run(content: string) {
 
     // console.debug(max)
 
-    const cubes = new Set<number>(input.map(([x, y, z]) =>  (x + 1) * max * max + (y + 1) * max + (z + 1)))
+    const cubes = new Set<number>(input.map(([x, y, z]) => toCube(max, [x+1, y+1, z+1])))
 
     let total = 0
     for (let cube of cubes) {
-        const x = Math.floor(cube / (max * max))
-        const y = Math.floor((cube - x * max * max) / max)
-        const z = cube - x * max * max - y * max
-        total += 6 - incs.filter(([xinc, yinc, zinc]) => cubes.has((x + xinc) * max * max + (y + yinc) * max + (z + zinc))).length
+        const [x, y, z] = fromCube(max, cube)
+        total += 6 - incs.filter(([xinc, yinc, zinc]) => cubes.has(toCube(max, [x + xinc, y + yinc, z + zinc]))).length
     }
 
     console.debug(total)
 
 };
 
-run(sampleContent); // 64 (sample)
-run(inputContent); // 4242
+run1(sampleContent); // 64 (sample)
+run1(inputContent); // 4242
