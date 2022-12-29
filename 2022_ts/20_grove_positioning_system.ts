79054dec555696159f3002658be68224d6a93b96
Leo Antoli <leoantol@amazon.com>
2022-12-29 14:36:54 +0100

    day 20 part 2

--- 2022_ts/20_grove_positioning_system.ts
+++ 2022_ts/20_grove_positioning_system.ts
@@ -3,43 +3,31 @@ import { readFileSync } from 'fs'
 const inputContent = readFileSync("inputs/20_input.txt", 'utf-8')
 const sampleContent = readFileSync("inputs/20_sample.txt", 'utf-8')
 
-function run(content: string) {
+function run(content: string, decryptionKey: number, rounds: number) {
     const lines = content.split('\n')
     lines.pop() // Remove last empty line
 
-    let list = lines.map((line, index) => [Number(line), index])
-    //list = list.slice(0, 10)
-    // console.debug(list)
-
+    let list = lines.map((line, index) => [Number(line) * decryptionKey, index])
     const mod = list.length
     
-    console.debug("before", list.map(([n, _]) => n))
-    for (let i = 0; i < list.length; i++) {
-        const initIdx = list.findIndex(([_, index]) => index === i)!
-        const initN = list[initIdx][0]
-        const n = list[initIdx][0] % (mod -1)
-        let finalIdx = initIdx + n
-        if (finalIdx > mod - 1) finalIdx -= mod - 1
-        if (finalIdx <= 0) finalIdx += mod - 1
-        const dir = Math.sign(finalIdx - initIdx)
-        for (let times = 0; times < Math.abs(finalIdx - initIdx); times++) {
-            [list[initIdx + dir * times], list[initIdx + dir * (times + 1)]] = [list[initIdx + dir * (times + 1)], list[initIdx + dir * times]]
+    for (let round = 0; round < rounds; round++) {
+        for (let i = 0; i < list.length; i++) {
+            const ini = list.findIndex(([_, index]) => index === i)
+            const n = list[ini][0] % (mod - 1)
+            const end = (ini + n + mod - 1) % (mod - 1)
+            const dir = Math.sign(end - ini)
+            for (let pos = ini; pos != end; pos += dir) {
+                [list[pos], list[pos + dir]] = [list[pos + dir], list[pos]]
+            }
         }
-        console.debug("middle", initN, list.map(([n, _]) => n))
     }
-    console.debug("after", list.map(([n, _]) => n))
 
     const idx0 = list.findIndex(([n, _]) => n === 0)
-
-    const n1 = list[(1000 + idx0) % mod][0]
-    const n2 = list[(2000 + idx0) % mod][0]
-    const n3 = list[(3000 + idx0) % mod][0]
-
-    console.debug("indexes", mod, idx0, (1000 + idx0) % mod, (2000 + idx0) % mod, (3000 + idx0) % mod)
-
-    console.debug(mod, n1, n2, n3, n1 + n2 + n3)
+    const sum = [1000, 2000, 3000].map(n => list[(n + idx0) % mod][0]).reduce((a, b) => a + b)
+    console.debug(sum)
 }
 
-run(sampleContent); // 3 (sample)
-run(inputContent); // 3700
-
+run(sampleContent, 1, 1);               // 3 (sample)
+run(sampleContent, 811589153, 10);      // 1623178306 (sample)
+run(inputContent, 1, 1);                // 3700
+run(inputContent, 811589153, 10);       // 10626948369382
