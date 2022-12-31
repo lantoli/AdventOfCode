902e204f8bb8bba1e8a528c13d3a98ea427747e1
Leo Antoli <leoantol@amazon.com>
2022-12-31 16:01:42 +0100

    day 22 clean

--- 2022_ts/22_monkey_map.ts
+++ 2022_ts/22_monkey_map.ts
@@ -1,5 +1,6 @@
-// Part 2 not done yet
-import { appendFile, readFileSync } from 'fs'
+// Note: wrapInput assumes the face layout in my input, it may fail for other input layouts. 
+// I created the die in paper to figure out the wrapping rules.
+import { readFileSync } from 'fs'
 import assert from 'assert';
 
 const inputContent = readFileSync("inputs/22_input.txt", 'utf-8')
@@ -26,11 +27,10 @@ function getBoard(content: string, size: number) {
     grid[i] += ' '.repeat(cols - lines[i].length)
     assert(grid[i].length == cols)
   }
-  // console.debug("check sizes",faceRows, faceCols, rows, cols)
   return { grid, faceRows, faceCols, rows, cols, path }
 }
 
-function wrapSample(info: Info, size: number) : Info | undefined {
+function wrapSample(info: Info, size: number) : Info {
   const { dir, ycube, xcube, yposcube, xposcube } = info
   if (ycube == 1 && xcube == 3 && xposcube == 0 && dir === 0)
     return { dir: 1, ycube: ycube + 1, xcube, yposcube: xposcube, xposcube: size - yposcube - 1 }
@@ -41,10 +41,10 @@ function wrapSample(info: Info, size: number) : Info | undefined {
   if (ycube == 0 && xcube == 1 && yposcube == size - 1 && dir == 3) 
     return { dir: 0, ycube, xcube: xcube + 1, yposcube: xposcube, xposcube: 0 }
 
-  return undefined
+    throw new Error("Wrapping not supported")
 }
 
-function wrapInput(info: Info, size: number) : Info | undefined {
+function wrapInput(info: Info, size: number) : Info {
   const { dir, ycube, xcube, yposcube, xposcube } = info
   if (ycube == 3 && xcube == 1 && yposcube == size - 1 && dir === 3)
     return { dir: 0, ycube: 3, xcube: 0, yposcube: xposcube, xposcube: 0 }
@@ -88,15 +88,12 @@ function wrapInput(info: Info, size: number) : Info | undefined {
   if (ycube == 0 && xcube == 0 && xposcube == 0 && dir === 0)
     return { dir: 2, ycube: 2, xcube: 1, yposcube: size - yposcube - 1, xposcube: size - 1 }
 
-  return undefined
+  throw new Error("Wrapping not supported")
 }
 
-
-function run(content: string, size: number, wrap?: (info: Info, size: number) => Info | undefined) {
+function run(content: string, size: number, wrap?: (info: Info, size: number) => Info) {
   const { grid, faceRows, faceCols, rows, cols, path } = getBoard(content, size)
   let ylast = 0, xlast = grid[0].indexOf('.'), dirlast = 0 // DEJAR ESTE
-  // let ylast = 5, xlast = 6, dirlast = 3
-  // console.debug("INI", ylast+1, xlast+1, dirlast)
 
   for (let i = 0; i < path.length; ) switch(path[i]) {
     case 'L': dirlast = (dirlast + 3) % 4; i++; break;
@@ -105,8 +102,6 @@ function run(content: string, size: number, wrap?: (info: Info, size: number) =>
       for (var moves = 0; i < path.length && path[i] !== 'L' && path[i] !== 'R'; i++) {
         moves = moves * 10 + path[i].charCodeAt(0) - '0'.charCodeAt(0)
       }
-
-      //console.debug("last i", i, path.length)
 
       const dirborrar = dirlast, xborrar = xlast, yborrar = ylast
       for (let move = 0, y = ylast, x = xlast, dir = dirlast; move < moves; move++, ylast = y, xlast = x, dirlast = dir) {
@@ -122,34 +117,23 @@ function run(content: string, size: number, wrap?: (info: Info, size: number) =>
             let ycube = Math.floor(y / size), xcube = Math.floor(x / size)
             let yposcube = y % size, xposcube = x % size
 
-            // console.debug("move", move, y, x, dir, grid[y][x], "cube", ycube, xcube, yposcube, xposcube)
-
             if ( (overflow || grid[y][x] === ' ') && 
                  (yposcube === 0 || yposcube === size - 1 || xposcube === 0 || xposcube === size - 1 ) ) {
 
               const info = wrap({ dir, ycube, xcube, yposcube, xposcube }, size)
-              if (typeof info !== "undefined") {
-                y = info.ycube * size + info.yposcube
-                x = info.xcube * size + info.xposcube
-                dir = info.dir
-              } else {
-                console.debug("NO RULE", ycube, xcube, yposcube, xposcube, dir, y+1, x+1, grid[y][x])
-              }
+              y = info.ycube * size + info.yposcube
+              x = info.xcube * size + info.xposcube
+              dir = info.dir
             }
             assert (grid[y][x] !== ' ')
           }
 
         } while (grid[y][x] === ' ')
-        // console.debug("move ini adj", move, y, x, yend, xend)
         if (grid[y][x] === '#') break
       }
-      // console.debug("moves", moves, "coord", y+1, x+1)
-      // console.debug("pos", y, x)
-      // console.debug("from", yborrar+1, xborrar+1, dirborrar, "to", ylast+1, xlast+1, dirlast)
       break;
   }
-
-  console.debug(ylast+1, xlast+1, dirlast, 1000 * (ylast+1) + 4 * (xlast+1) + dirlast)
+  console.debug(1000 * (ylast+1) + 4 * (xlast+1) + dirlast)
 }
 
 run(sampleContent, 4);                // 6032 (sample)
