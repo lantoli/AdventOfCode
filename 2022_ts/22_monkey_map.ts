259be7512dffab6a9707eddab07461a65facecee
Leo Antoli <leoantol@amazon.com>
2022-12-31 11:12:41 +0100

    day 22 part 2 in example working, not yet in input

--- 2022_ts/22_monkey_map.ts
+++ 2022_ts/22_monkey_map.ts
@@ -1,5 +1,5 @@
 // Part 2 not done yet
-import { readFileSync } from 'fs'
+import { appendFile, readFileSync } from 'fs'
 import assert from 'assert';
 
 const inputContent = readFileSync("inputs/22_input.txt", 'utf-8')
@@ -28,38 +28,100 @@ function getBoard(content: string, size: number) {
   return { grid, faceRows, faceCols, rows, cols, path }
 }
 
-function run(content: string, size: number) {
+function run(content: string, size: number, isCube: boolean) {
   const { grid, faceRows, faceCols, rows, cols, path } = getBoard(content, size)
-  let y = 0, x = grid[0].indexOf('.'), dir = 0
+  let ylast = 0, xlast = grid[0].indexOf('.'), dirlast = 0 // DEJAR ESTE
+  // let ylast = 5, xlast = 6, dirlast = 3
+  console.debug("INI", ylast+1, xlast+1, dirlast)
 
   for (let i = 0; i < path.length; ) switch(path[i]) {
-    case 'L': dir = (dir + 3) % 4; i++; break;
-    case 'R': dir = (dir + 1) % 4; i++; break;
+    case 'L': dirlast = (dirlast + 3) % 4; i++; break;
+    case 'R': dirlast = (dirlast + 1) % 4; i++; break;
     default: 
       for (var moves = 0; i < path.length && path[i] !== 'L' && path[i] !== 'R'; i++) {
         moves = moves * 10 + path[i].charCodeAt(0) - '0'.charCodeAt(0)
       }
-      for (let move = 0, yend = y, xend = x; move < moves; move++, y = yend, x = xend) {
-        // console.debug("move ini", move, y, x, yend, xend, board[yend][xend])
+      const dirborrar = dirlast, xborrar = xlast, yborrar = ylast
+      for (let move = 0, y = ylast, x = xlast, dir = dirlast; move < moves; move++, ylast = y, xlast = x, dirlast = dir) {
         do {
-          yend += incs[dir][0]; xend += incs[dir][1]
-          if (yend == -1) yend = rows - 1
-          if (yend == rows) yend = 0
-          if (xend == -1) xend = cols - 1
-          if (xend == cols) xend = 0
-        } while (grid[yend][xend] === ' ')
+          y += incs[dir][0]; x += incs[dir][1]
+          let overflow = false
+          if (y == -1) { y = rows - 1; overflow = true }
+          if (y == rows) { y = 0; overflow = true }
+          if (x == -1) { x = cols - 1; overflow = true }
+          if (x == cols) { x = 0; overflow = true }
+
+          if (isCube) {
+            let ycube = Math.floor(y / size), xcube = Math.floor(x / size)
+            let yposcube = y % size, xposcube = x % size
+
+            // console.debug("move", move, y, x, dir, grid[y][x], "cube", ycube, xcube, yposcube, xposcube)
+
+            if ( (overflow || grid[y][x] === ' ') && 
+                 (yposcube === 0 || yposcube === size - 1 || xposcube === 0 || xposcube === size - 1 ) ) {
+              let applied = false
+
+              if (ycube == 1 && xcube == 3 && xposcube == 0) {
+                applied = true
+                console.debug("ACTIVANDO 1", ycube, xcube, yposcube, xposcube, dir, y+1, x+1)
+                assert(dir === 0)
+                xposcube = size - yposcube - 1
+                yposcube = 0
+                ycube++
+
+                dir = 1
+                y = ycube * size + yposcube
+                x = xcube * size + xposcube
+                console.debug("NUEVO", dir, y+1, x+1)
+              }
+
+              if (ycube == 0 && xcube == 2 && yposcube == 0) {
+                applied = true
+                console.debug("ACTIVANDO 2", ycube, xcube, yposcube, xposcube, dir, y+1, x+1)
+                assert(dir === 1)
+                yposcube = size - 1
+                xposcube = size - xposcube - 1
+                ycube = 1
+                xcube = 0
+
+                dir = 3
+                y = ycube * size + yposcube
+                x = xcube * size + xposcube
+                console.debug("NUEVO", dir, y+1, x+1)
+              }
+
+              if (ycube == 0 && xcube == 1 && yposcube == size - 1) {
+                applied = true
+                console.debug("ACTIVANDO 3", ycube, xcube, yposcube, xposcube, dir, y+1, x+1)
+                assert(dir === 3)
+                yposcube = xposcube
+                xposcube = 0
+                xcube++
+
+                dir = 0
+                y = ycube * size + yposcube
+                x = xcube * size + xposcube
+                console.debug("NUEVO", dir, y+1, x+1, grid[y][x])
+              }
+              if (!applied) {
+                console.debug("NO RULE", ycube, xcube, yposcube, xposcube, dir, y+1, x+1, grid[y][x])
+              }
+            }
+          }
+        } while (!isCube && grid[y][x] === ' ')
         // console.debug("move ini adj", move, y, x, yend, xend)
-        if (grid[yend][xend] === '#') break
+        if (grid[y][x] === '#') break
       }
       // console.debug("moves", moves, "coord", y+1, x+1)
       // console.debug("pos", y, x)
+      console.debug("from", yborrar+1, xborrar+1, dirborrar, "to", ylast+1, xlast+1, dirlast)
       break;
   }
 
-  console.debug(y+1, x+1, dir, 1000 * (y+1) + 4 * (x+1) + dir)
+  console.debug(ylast+1, xlast+1, dirlast, 1000 * (ylast+1) + 4 * (xlast+1) + dirlast)
 }
 
-run(sampleContent, 4); // 6032 (sample)
-run(inputContent, 50); // 109094
+// run(sampleContent, 4, false); // 6032 (sample)
+// run(inputContent, 50, false); // 109094
 
-run(sampleContent, 4); // 5031 (sample) PART 2
+run(sampleContent, 4, true); // 5031 (sample) PART 2
