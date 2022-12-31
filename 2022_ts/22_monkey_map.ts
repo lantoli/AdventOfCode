5e30f3095fb3ee40364bd533a8fc0f4efd09fafd
Leo Antoli <leoantol@amazon.com>
2022-12-31 11:39:56 +0100

    day 22 preparing part 2

--- 2022_ts/22_monkey_map.ts
+++ 2022_ts/22_monkey_map.ts
@@ -5,6 +5,8 @@ import assert from 'assert';
 const inputContent = readFileSync("inputs/22_input.txt", 'utf-8')
 const sampleContent = readFileSync("inputs/22_sample.txt", 'utf-8')
 
+type Info = { dir: number, ycube: number, xcube: number, yposcube: number, xposcube:number }
+
 const incs = [ [0, 1], [1, 0], [0, -1], [-1, 0]  ];
 
 function getBoard(content: string, size: number) {
@@ -28,11 +30,30 @@ function getBoard(content: string, size: number) {
   return { grid, faceRows, faceCols, rows, cols, path }
 }
 
-function run(content: string, size: number, isCube: boolean) {
+function wrapSample(info: Info, size: number) : Info | undefined {
+  const { dir, ycube, xcube, yposcube, xposcube } = info
+  if (ycube == 1 && xcube == 3 && xposcube == 0 && dir === 0)
+    return { dir: 1, ycube: ycube + 1, xcube, yposcube: xposcube, xposcube: size - yposcube - 1 }
+  
+  if (ycube == 0 && xcube == 2 && yposcube == 0 && dir == 1) 
+    return { dir: 3, ycube: ycube + 1, xcube: 0, yposcube: size - 1, xposcube: size - xposcube - 1 }
+
+  if (ycube == 0 && xcube == 1 && yposcube == size - 1 && dir == 3) 
+    return { dir: 0, ycube, xcube: xcube + 1, yposcube: xposcube, xposcube: 0 }
+
+  return undefined
+}
+
+function wrapInput(info: Info, size: number) : Info | undefined {
+  return undefined
+}
+
+
+function run(content: string, size: number, wrap?: (info: Info, size: number) => Info | undefined) {
   const { grid, faceRows, faceCols, rows, cols, path } = getBoard(content, size)
   let ylast = 0, xlast = grid[0].indexOf('.'), dirlast = 0 // DEJAR ESTE
   // let ylast = 5, xlast = 6, dirlast = 3
-  console.debug("INI", ylast+1, xlast+1, dirlast)
+  // console.debug("INI", ylast+1, xlast+1, dirlast)
 
   for (let i = 0; i < path.length; ) switch(path[i]) {
     case 'L': dirlast = (dirlast + 3) % 4; i++; break;
@@ -51,7 +72,7 @@ function run(content: string, size: number, isCube: boolean) {
           if (x == -1) { x = cols - 1; overflow = true }
           if (x == cols) { x = 0; overflow = true }
 
-          if (isCube) {
+          if (typeof wrap !== "undefined") {
             let ycube = Math.floor(y / size), xcube = Math.floor(x / size)
             let yposcube = y % size, xposcube = x % size
 
@@ -59,69 +80,34 @@ function run(content: string, size: number, isCube: boolean) {
 
             if ( (overflow || grid[y][x] === ' ') && 
                  (yposcube === 0 || yposcube === size - 1 || xposcube === 0 || xposcube === size - 1 ) ) {
-              let applied = false
 
-              if (ycube == 1 && xcube == 3 && xposcube == 0) {
-                applied = true
-                console.debug("ACTIVANDO 1", ycube, xcube, yposcube, xposcube, dir, y+1, x+1)
-                assert(dir === 0)
-                xposcube = size - yposcube - 1
-                yposcube = 0
-                ycube++
-
-                dir = 1
-                y = ycube * size + yposcube
-                x = xcube * size + xposcube
-                console.debug("NUEVO", dir, y+1, x+1)
-              }
-
-              if (ycube == 0 && xcube == 2 && yposcube == 0) {
-                applied = true
-                console.debug("ACTIVANDO 2", ycube, xcube, yposcube, xposcube, dir, y+1, x+1)
-                assert(dir === 1)
-                yposcube = size - 1
-                xposcube = size - xposcube - 1
-                ycube = 1
-                xcube = 0
-
-                dir = 3
-                y = ycube * size + yposcube
-                x = xcube * size + xposcube
-                console.debug("NUEVO", dir, y+1, x+1)
-              }
-
-              if (ycube == 0 && xcube == 1 && yposcube == size - 1) {
-                applied = true
-                console.debug("ACTIVANDO 3", ycube, xcube, yposcube, xposcube, dir, y+1, x+1)
-                assert(dir === 3)
-                yposcube = xposcube
-                xposcube = 0
-                xcube++
-
-                dir = 0
-                y = ycube * size + yposcube
-                x = xcube * size + xposcube
-                console.debug("NUEVO", dir, y+1, x+1, grid[y][x])
-              }
-              if (!applied) {
+              const info = wrap({ dir, ycube, xcube, yposcube, xposcube }, size)
+              if (typeof info !== "undefined") {
+                y = info.ycube * size + info.yposcube
+                x = info.xcube * size + info.xposcube
+                dir = info.dir
+              } else {
                 console.debug("NO RULE", ycube, xcube, yposcube, xposcube, dir, y+1, x+1, grid[y][x])
               }
             }
+            assert (grid[y][x] !== ' ')
           }
-        } while (!isCube && grid[y][x] === ' ')
+
+        } while (grid[y][x] === ' ')
         // console.debug("move ini adj", move, y, x, yend, xend)
         if (grid[y][x] === '#') break
       }
       // console.debug("moves", moves, "coord", y+1, x+1)
       // console.debug("pos", y, x)
-      console.debug("from", yborrar+1, xborrar+1, dirborrar, "to", ylast+1, xlast+1, dirlast)
+      // console.debug("from", yborrar+1, xborrar+1, dirborrar, "to", ylast+1, xlast+1, dirlast)
       break;
   }
 
   console.debug(ylast+1, xlast+1, dirlast, 1000 * (ylast+1) + 4 * (xlast+1) + dirlast)
 }
 
-// run(sampleContent, 4, false); // 6032 (sample)
-// run(inputContent, 50, false); // 109094
+run(sampleContent, 4); // 6032 (sample)
+run(inputContent, 50); // 109094
 
-run(sampleContent, 4, true); // 5031 (sample) PART 2
+run(sampleContent, 4, wrapSample); // 5031 (sample) PART 2
+// run(inputContent, 50, wrapInput); // ... PART 2
