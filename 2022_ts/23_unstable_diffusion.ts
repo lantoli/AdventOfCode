08cefdda55cfdee25c92e94efbbfd8ff91ebbc85
Leo Antoli <leoantol@amazon.com>
2023-01-01 23:30:18 +0100

    day 23 part 2

--- 2022_ts/23_unstable_diffusion.ts
+++ 2022_ts/23_unstable_diffusion.ts
@@ -11,15 +11,14 @@ const dirs = [
     [[0, 1] , [-1, 1] , [1, 1]],
 ]
 const dirsFlat = dirs.flat()
-
+const roundsPart1 = 10
+const maxRounds = 2000
 
 function run(content: string) {
     const lines = content.split('\n')
     lines.pop() // Remove last empty line
-
-    const rounds = 10
     
-    const inc = rounds * 2
+    const inc = maxRounds * 2
     assert (lines.length === lines[0].length)
     const dim = lines.length
     const mod = dim + inc * 3
@@ -45,14 +44,33 @@ function run(content: string) {
         }
     }
 
+    function part1(map: Set<number>) {
+        let ymin = Infinity, ymax = 0, xmin = Infinity, xmax = 0
+        for (const elf of map) {
+            const [y, x] = toCoord(elf)
+            ymin = Math.min(ymin, y)
+            ymax = Math.max(ymax, y)
+            xmin = Math.min(xmin, x)
+            xmax = Math.max(xmax, x)
+        }
+        let total = 0
+        for (let y = ymin; y <= ymax; y++) for (let x = xmin; x <= xmax; x++) {
+            if (!map.has(fromCoord(y, x))) total++;
+        }
+        //console.debug("coords", ymin, xmin, ymax, xmax ,"total", total)
+        return total
+     }   
+    
     const map = new Set<number>()
     for (let y = 0; y < dim; y++) for (let x = 0; x < dim; x++) {
         if (lines[y][x] === '#') map.add(fromCoord(y, x))
     }
 
-    displayMap("INIT", map)
+    //displayMap("INIT", map)
+    let prevMap = new Set<number>()
 
-    for (let round = 1, dirIndex = 0; round <= rounds; round++, dirIndex = (dirIndex + 1) % dirs.length) {
+    for (let round = 1, dirIndex = 0; round <= maxRounds; round++, dirIndex = (dirIndex + 1) % dirs.length) {
+
         const proposed = new Map<number, number>()
         for (const current of map) {
             const [y, x] = toCoord(current)
@@ -63,46 +81,38 @@ function run(content: string) {
                         const next = fromCoord(y + dir[0][0], x + dir[0][1])
                         if (proposed.has(next)) {
                             proposed.set(next, -1)
-                            console.debug("ELF BLOCKED", toCoord(current), toCoord(next))
+                            //console.debug("ELF BLOCKED", toCoord(current), toCoord(next))
                         } else {
                             proposed.set(next, current)
-                            console.debug("ELF MOVE", toCoord(current), toCoord(next))
+                            //console.debug("ELF MOVE", toCoord(current), toCoord(next))
                         }
                         break
                     }
                 }
             } else {
-                console.debug("ELF DONT MOVE", toCoord(current))
+                //console.debug("ELF DONT MOVE", toCoord(current))
             }
         }
         for (const [next, current] of proposed) if (current !== -1) {
-            console.debug("ELF MOVING", toCoord(current), toCoord(next))
+            //console.debug("ELF MOVING", toCoord(current), toCoord(next))
             map.delete(current)
             map.add(next)
         }
-        displayMap("ROUND " + round, map)
-    }
-    let ymin = Infinity, ymax = 0, xmin = Infinity, xmax = 0
-    for (const elf of map) {
-        const [y, x] = toCoord(elf)
-        ymin = Math.min(ymin, y)
-        ymax = Math.max(ymax, y)
-        xmin = Math.min(xmin, x)
-        xmax = Math.max(xmax, x)
-    }
+        // displayMap("ROUND " + round, map)
+        if (round === roundsPart1) console.debug(part1(map))
 
-    let total = 0
-    for (let y = ymin; y <= ymax; y++) for (let x = xmin; x <= xmax; x++) {
-        if (!map.has(fromCoord(y, x))) total++;
+        if ([...map].every(elf => prevMap.has(elf))) {
+            console.debug(round)
+            break
+        } else prevMap = new Set(map)
     }
-
-    displayMap("END ", map)
-    console.debug("coords", ymin, xmin, ymax, xmax ,"total", total)
-
+    
+    // displayMap("END ", map)
+    
     //for (const elf of map) console.debug(elf, toCoord(elf))
     //console.debug(rounds, inc, dim, mod, "count", map.size)
     
 }
 
-run(sampleContent) // 110 (sample)
-run(inputContent) // 4000
+run(sampleContent) // 110, 20 (sample)
+run(inputContent) // 4000, 1040
