cd7beab377402b81c3c3c5f482bcfda8dcfc9f1d
Leo Antoli <430982+lantoli@users.noreply.github.com>
2023-12-02 23:04:25 +0100

    01

---
+++ 2023_go/01_trebuchet.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"go.openly.dev/pointy"
+)
+
+const filepath = "inputs/01_input.txt"
+
+func main() {
+	fmt.Println(calc(filepath, false)) // 55208
+	fmt.Println(calc(filepath, true))  // 54578
+}
+
+func calc(filepath string, includeLetters bool) int {
+	var total int
+	content, _ := os.ReadFile(filepath)
+	lines := bytes.Split(content, []byte("\n"))
+	for i := 0; i < len(lines)-1; i++ {
+		var first *int
+		var last int
+		for pos := range lines[i] {
+			if digit := getDigit(pos, lines[i], includeLetters); digit != nil {
+				if first == nil {
+					first = digit
+				}
+				last = *digit
+			}
+		}
+		total += *first*10 + last
+	}
+	return total
+}
+
+func getDigit(pos int, line []byte, includeLetter bool) *int {
+	char := line[pos]
+	if char >= '0' && char <= '9' {
+		return pointy.Int(int(char - '0'))
+	}
+	if !includeLetter {
+		return nil
+	}
+	for n, word := range []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"} {
+		if bytes.HasPrefix(line[pos:], []byte(word)) {
+			return pointy.Int(n)
+		}
+	}
+	return nil
+}
--- 2023_go/go.mod
+++ 2023_go/go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.21
+
+require go.openly.dev/pointy v1.3.0
