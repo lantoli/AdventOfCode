a251dd1ea0ba1552c4194a342105637051d1f0c7
Leo Antoli <430982+lantoli@users.noreply.github.com>
2023-12-10 19:00:45 +0100

    10 clean-up

--- 2023_go/10_maze.go
+++ 2023_go/10_maze.go
@@ -10,11 +10,10 @@ const filepath10 = "inputs/10_input.txt"
 
 func main() {
 	fmt.Println(calc10(false)) // 7093
-	fmt.Println(calc10(true))  // 393 That's not the right answer; your answer is too low. Curiously, it's the right answer for someone else; you might be logged in to the wrong account or just unlucky.
-	// 3844 That's not the right answer; your answer is too high. If you're stuck, 
+	fmt.Println(calc10(true))  // 407 (ret: 3844, 3844, 407 , 407)
 }
 
-func calc10(enclosed bool) int {
+func calc10(enclosed bool) []int {
 	f, _ := os.Open(filepath10)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
@@ -29,31 +28,30 @@ func calc10(enclosed bool) int {
 	for row := 0; row < rows; row++ {
 		for col := 0; col < cols; col++ {
 			if maze[row][col] == 'S' {
-				source = state{row, col}
+				source = state{row: row, col: col}
 				break
 			}
 		}
 	}
-	fmt.Println("S", source)
 	total := 0
 	states := append([]state{}, source)
 	var sinfo info
 	visited := make(map[state]bool)
 	visited[source] = true
 	if source.row > 0 && infos[maze[source.row-1][source.col]].s {
-		states = append(states, state{source.row - 1, source.col})
+		states = append(states, state{row: source.row - 1, col: source.col})
 		sinfo.n = true
 	}
 	if infos[maze[source.row+1][source.col]].n {
-		states = append(states, state{source.row + 1, source.col})
+		states = append(states, state{row: source.row + 1, col: source.col})
 		sinfo.s = true
 	}
 	if infos[maze[source.row][source.col+1]].w {
-		states = append(states, state{source.row, source.col + 1})
+		states = append(states, state{row: source.row, col: source.col + 1})
 		sinfo.e = true
 	}
 	if source.col > 0 && infos[maze[source.row][source.col-1]].e {
-		states = append(states, state{source.row, source.col - 1})
+		states = append(states, state{row: source.row, col: source.col - 1})
 		sinfo.w = true
 	}
 
@@ -69,16 +67,16 @@ func calc10(enclosed bool) int {
 				visited[s] = true
 				i := infos[maze[s.row][s.col]]
 				if i.n {
-					newStates = append(newStates, state{s.row - 1, s.col})
+					newStates = append(newStates, state{row: s.row - 1, col: s.col})
 				}
 				if i.s {
-					newStates = append(newStates, state{s.row + 1, s.col})
+					newStates = append(newStates, state{row: s.row + 1, col: s.col})
 				}
 				if i.e {
-					newStates = append(newStates, state{s.row, s.col + 1})
+					newStates = append(newStates, state{row: s.row, col: s.col + 1})
 				}
 				if i.w {
-					newStates = append(newStates, state{s.row, s.col - 1})
+					newStates = append(newStates, state{row: s.row, col: s.col - 1})
 				}
 			}
 		}
@@ -89,23 +87,23 @@ func calc10(enclosed bool) int {
 		total++
 	}
 	if enclosed {
-		total = 0
-		fmt.Println("Cluster", countCluster(visited, maze, extstate{source.row, source.col, false, true, true, false}))
-		fmt.Println("Cluster", countCluster(visited, maze, extstate{source.row, source.col, true, false, true, false}))
-		fmt.Println("Cluster", countCluster(visited, maze, extstate{source.row, source.col, false, true, false, true}))
-		fmt.Println("Cluster", countCluster(visited, maze, extstate{source.row, source.col, true, false, false, true}))
+		return []int{
+			countCluster(visited, maze, state{source.row, source.col, false, true, true, false}),
+			countCluster(visited, maze, state{source.row, source.col, true, false, true, false}),
+			countCluster(visited, maze, state{source.row, source.col, false, true, false, true}),
+			countCluster(visited, maze, state{source.row, source.col, true, false, false, true}),
+		}
+	} else {
+		return []int{total}
 	}
-
-	return total
 }
 
-func countCluster(walls map[state]bool, maze [][]byte, ini extstate) int {
+func countCluster(walls map[state]bool, maze [][]byte, ini state) int {
 	rows := len(maze)
 	cols := len(maze[0])
-	//walled
-	visited := make(map[extstate]bool)
+	visited := make(map[state]bool)
 	count := make(map[state]bool)
-	states := append([]extstate{}, ini)
+	states := append([]state{}, ini)
 	for len(states) > 0 {
 		s := states[len(states)-1]
 		states = states[:len(states)-1]
@@ -113,42 +111,41 @@ func countCluster(walls map[state]bool, maze [][]byte, ini extstate) int {
 			continue
 		}
 		visited[s] = true
-		if !walls[state{s.row, s.col}] {
-			count[state{s.row, s.col}] = true
+		if !walls[state{row: s.row, col: s.col}] {
+			count[state{row: s.row, col: s.col}] = true
 		}
 
 		if s.s && s.row+1 < rows {
 			i := infos[maze[s.row+1][s.col]]
-			states = append(states, extstate{s.row + 1, s.col, !s.n, !s.s, s.e, s.w})
-			if (s.w && !i.w) || (s.e && !i.e) || !walls[state{s.row + 1, s.col}] {
-				states = append(states, extstate{s.row + 1, s.col, s.n, s.s, s.e, s.w})
+			states = append(states, state{s.row + 1, s.col, !s.n, !s.s, s.e, s.w})
+			if (s.w && !i.w) || (s.e && !i.e) || !walls[state{row: s.row + 1, col: s.col}] {
+				states = append(states, state{s.row + 1, s.col, s.n, s.s, s.e, s.w})
 			}
 		}
 
 		if s.n && s.row-1 >= 0 {
 			i := infos[maze[s.row-1][s.col]]
-			states = append(states, extstate{s.row - 1, s.col, !s.n, !s.s, s.e, s.w})
-			if (s.w && !i.w) || (s.e && !i.e) || !walls[state{s.row - 1, s.col}] {
-				states = append(states, extstate{s.row - 1, s.col, s.n, s.s, s.e, s.w})
+			states = append(states, state{s.row - 1, s.col, !s.n, !s.s, s.e, s.w})
+			if (s.w && !i.w) || (s.e && !i.e) || !walls[state{row: s.row - 1, col: s.col}] {
+				states = append(states, state{s.row - 1, s.col, s.n, s.s, s.e, s.w})
 			}
 		}
 
 		if s.e && s.col+1 < cols {
 			i := infos[maze[s.row][s.col+1]]
-			states = append(states, extstate{s.row, s.col + 1, s.n, s.s, !s.e, !s.w})
-			if (s.n && !i.n) || (s.s && !i.s) || !walls[state{s.row, s.col + 1}] {
-				states = append(states, extstate{s.row, s.col + 1, s.n, s.s, s.e, s.w})
+			states = append(states, state{s.row, s.col + 1, s.n, s.s, !s.e, !s.w})
+			if (s.n && !i.n) || (s.s && !i.s) || !walls[state{row: s.row, col: s.col + 1}] {
+				states = append(states, state{s.row, s.col + 1, s.n, s.s, s.e, s.w})
 			}
 		}
 
 		if s.w && s.col-1 >= 0 {
 			i := infos[maze[s.row][s.col-1]]
-			states = append(states, extstate{s.row, s.col - 1, s.n, s.s, !s.e, !s.w})
-			if (s.n && !i.n) || (s.s && !i.s) || !walls[state{s.row, s.col - 1}] {
-				states = append(states, extstate{s.row, s.col - 1, s.n, s.s, s.e, s.w})
+			states = append(states, state{s.row, s.col - 1, s.n, s.s, !s.e, !s.w})
+			if (s.n && !i.n) || (s.s && !i.s) || !walls[state{row: s.row, col: s.col - 1}] {
+				states = append(states, state{s.row, s.col - 1, s.n, s.s, s.e, s.w})
 			}
 		}
-
 	}
 	return len(count)
 }
@@ -167,10 +164,6 @@ type info struct {
 }
 
 type state struct {
-	row, col int
-}
-
-type extstate struct {
 	row, col   int
 	n, s, e, w bool
 }
