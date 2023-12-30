cb0ed5d699255d9f78ac4424237845e0fe49f42a
Leo Antoli <430982+lantoli@users.noreply.github.com>
2023-12-30 11:21:08 +0100

    13 part 2

--- 2023_go/13_incidence.go
+++ 2023_go/13_incidence.go
@@ -9,31 +9,50 @@ import (
 const filepath13 = "inputs/13_input.txt"
 
 func main() {
-	fmt.Println(calc13()) // 33728
+	fmt.Println(calc13(false)) // 33728
+	fmt.Println(calc13(true))  // 28235
 }
 
-func calc13() int {
+func calc13(smudge bool) int {
 	f, _ := os.Open(filepath13)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	total := 0
-	m := make([]string, 0)
+	m := make([][]byte, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
-			total += calc13Map(m)
-			m = make([]string, 0)
+			total += calc13Main(m, smudge)
+			m = make([][]byte, 0)
 		} else {
-			m = append(m, line)
+			m = append(m, []byte(line))
 		}
 	}
 	if len(m) > 0 {
-		total += calc13Map(m)
+		total += calc13Main(m, smudge)
 	}
 	return total
 }
 
-func calc13Map(m []string) int {
+func calc13Main(m [][]byte, smudge bool) int {
+	prev := calc13Map(m, 0)
+	if !smudge {
+		return prev
+	}
+	rows, cols := len(m), len(m[0])
+	for y := 0; y < rows; y++ {
+		for x := 0; x < cols; x++ {
+			swap(m, y, x)
+			if ret := calc13Map(m, prev); ret != 0 {
+				return ret
+			}
+			swap(m, y, x)
+		}
+	}
+	return 0
+}
+
+func calc13Map(m [][]byte, prev int) int {
 	rows, cols := len(m), len(m[0])
 outer1:
 	for x := 0; x+1 < cols; x++ {
@@ -44,7 +63,10 @@ outer1:
 				}
 			}
 		}
-		return x + 1
+		ret := x + 1
+		if ret != prev {
+			return ret
+		}
 	}
 outer2:
 	for y := 0; y+1 < rows; y++ {
@@ -55,7 +77,18 @@ outer2:
 				}
 			}
 		}
-		return (y + 1) * 100
+		ret := (y + 1) * 100
+		if ret != prev {
+			return ret
+		}
 	}
 	return 0
+}
+
+func swap(m [][]byte, y, x int) {
+	if m[y][x] == '.' {
+		m[y][x] = '#'
+	} else {
+		m[y][x] = '.'
+	}
 }
