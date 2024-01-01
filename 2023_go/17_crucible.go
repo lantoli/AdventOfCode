e809e6d49d1c0d0e5fff3ab59f059b52bb8dcd67
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-01-01 17:49:22 +0100

    17 part 1

--- 2023_go/17_crucible.go
+++ 2023_go/17_crucible.go
@@ -5,15 +5,17 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 )
 
-const filepath17 = "inputs/17_sample.txt"
+const filepath17 = "inputs/17_input.txt"
 
 func main() {
-	fmt.Println(calc17()) // XXX
+	fmt.Println(calc17()) // 861
 }
 
 var ret17 int
+var global17 map[st17]int
 
 func calc17() int {
 	f, _ := os.Open(filepath17)
@@ -27,46 +29,57 @@ func calc17() int {
 		panic("grid is not square")
 	}
 	ret17 = math.MaxInt
+	global17 = make(map[st17]int)
 	calc17Main(grid)
 	return ret17
 }
 
 func calc17Main(grid []string) {
-	calc17Rec(grid, 0, &[]st17{st17{0, 0, EAST, 0, len(grid)}})
-	//calc17Rec(grid, 0, &[]st17{st17{0, 0, SOUTH, len(grid)}})
+	calc17Rec(grid, 0, &st17{0, 0, EAST, 0, len(grid)}, map[st17key]any{})
+	calc17Rec(grid, 0, &st17{0, 0, SOUTH, 0, len(grid)}, map[st17key]any{})
 }
 
-func calc17Rec(grid []string, heat int, visited *[]st17) {
-	last := (*visited)[len(*visited)-1]
+func calc17Rec(grid []string, heat int, last *st17, v map[st17key]any) {
 	if last.y == len(grid)-1 && last.x == len(grid)-1 {
+		if heat < ret17 {
+			fmt.Println(heat)
+		}
 		ret17 = min(ret17, heat)
 		return
-	}
-	for i := 0; i < len(*visited)-1; i++ {
-		if (*visited)[i].y == last.y && (*visited)[i].x == last.x && (*visited)[i].dir == last.dir {
-			return
-		}
 	}
 	if heat >= ret17 {
 		return
 	}
-	copy := st17{last.y, last.x, last.dir, last.single, last.n}
+
+	if value, exist := global17[*last]; (last.x != 0 || last.y != 0) && exist && value <= heat {
+		return
+	}
+	global17[*last] = heat
+
+	candidates := make([]*st17, 0)
+	copy := &st17{last.y, last.x, last.dir, last.single, last.n}
 	if copy.move() {
-		*visited = append(*visited, copy)
-		calc17Rec(grid, heat+int(grid[copy.y][copy.x])-'0', visited)
-		*visited = (*visited)[:len(*visited)-1]
+		candidates = append(candidates, copy)
 	}
-	copy = st17{last.y, last.x, last.dir, last.single, last.n}
+	copy = &st17{last.y, last.x, last.dir, last.single, last.n}
 	if copy.moveLeft() {
-		*visited = append(*visited, copy)
-		calc17Rec(grid, heat+int(grid[copy.y][copy.x])-'0', visited)
-		*visited = (*visited)[:len(*visited)-1]
+		candidates = append(candidates, copy)
 	}
-	copy = st17{last.y, last.x, last.dir, last.single, last.n}
+	copy = &st17{last.y, last.x, last.dir, last.single, last.n}
 	if copy.moveRight() {
-		*visited = append(*visited, copy)
-		calc17Rec(grid, heat+int(grid[copy.y][copy.x])-'0', visited)
-		*visited = (*visited)[:len(*visited)-1]
+		candidates = append(candidates, copy)
+	}
+	sort.Slice(candidates, func(i, j int) bool {
+		return candidates[i].x+candidates[i].y >= candidates[j].x+candidates[j].y
+	})
+	for _, c := range candidates {
+		key := st17key{c.y, c.x, c.dir}
+		if _, exist := v[key]; exist {
+			return
+		}
+		v[key] = nil
+		calc17Rec(grid, heat+int(grid[c.y][c.x])-'0', c, v)
+		delete(v, key)
 	}
 }
 
@@ -76,6 +89,12 @@ type st17 struct {
 	dir    int
 	single int
 	n      int
+}
+
+type st17key struct {
+	y   int
+	x   int
+	dir int
 }
 
 func (s *st17) move() bool {
@@ -90,7 +109,7 @@ func (s *st17) move() bool {
 		s.x--
 	}
 	s.single++
-	return s.single <= 33 && s.y >= 0 && s.y < s.n && s.x >= 0 && s.x < s.n
+	return s.single <= 3 && s.y >= 0 && s.y < s.n && s.x >= 0 && s.x < s.n
 }
 
 func (s *st17) moveLeft() bool {
