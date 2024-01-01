279f5e2a565294800835c475c46593fa0b331ff7
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-01-01 19:28:27 +0100

    17 part 2

--- 2023_go/17_crucible.go
+++ 2023_go/17_crucible.go
@@ -11,7 +11,10 @@ import (
 const filepath17 = "inputs/17_input.txt"
 
 func main() {
+	ultra = false
 	fmt.Println(calc17()) // 861
+	ultra = true
+	fmt.Println(calc17()) // 1037
 }
 
 var (
@@ -19,6 +22,7 @@ var (
 	n        byte
 	ret17    int
 	global17 map[st17]int
+	ultra    bool
 )
 
 func calc17() int {
@@ -32,7 +36,6 @@ func calc17() int {
 	if len(grid) != len(grid[0]) {
 		panic("grid is not square")
 	}
-
 	n = byte(len(grid))
 	ret17 = math.MaxInt
 	global17 = make(map[st17]int)
@@ -50,11 +53,8 @@ func calc17Rec(grid []string, heat int, last *st17) {
 		return
 	}
 	global17[*last] = heat
-	if last.y == n-1 && last.x == n-1 {
-		if heat < ret17 {
-			ret17 = heat
-			fmt.Println(ret17)
-		}
+	if last.y == n-1 && last.x == n-1 && (!ultra || last.single >= 4) {
+		ret17 = min(ret17, heat)
 		return
 	}
 	candidates := make([]*st17, 0)
@@ -87,6 +87,10 @@ type st17 struct {
 }
 
 func (s *st17) move() bool {
+	var max byte = 3
+	if ultra {
+		max = 10
+	}
 	switch s.dir {
 	case NORTH:
 		s.y--
@@ -98,16 +102,22 @@ func (s *st17) move() bool {
 		s.x--
 	}
 	s.single++
-	return s.single <= 3 && s.y >= 0 && s.y < n && s.x >= 0 && s.x < n
+	return s.single <= max && s.y >= 0 && s.y < n && s.x >= 0 && s.x < n
 }
 
 func (s *st17) moveLeft() bool {
+	if ultra && s.single < 4 {
+		return false
+	}
 	s.dir = (s.dir - 1 + 4) % 4
 	s.single = 0
 	return s.move()
 }
 
 func (s *st17) moveRight() bool {
+	if ultra && s.single < 4 {
+		return false
+	}
 	s.dir = (s.dir + 1) % 4
 	s.single = 0
 	return s.move()
