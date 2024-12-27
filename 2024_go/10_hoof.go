f6e00545df8d357428c44301d47b8c0bba5fcd50
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-12-27 18:49:27 +0100

    2024 10

---
+++ 2024_go/10_hoof.go
@@ -0,0 +1,86 @@
+package main
+
+// 754 1609 (sample 36 81)
+func main() {
+	solve("10_input.txt", line10a, nil, solve10a, solve10b)
+}
+
+var (
+	input10    = make([][]int, 0)
+	rows, cols int
+)
+
+func line10a(line string) {
+	row := make([]int, len(line))
+	for i, ch := range line {
+		row[i] = int(ch) - '0'
+	}
+	input10 = append(input10, row)
+	rows = len(input10)
+	cols = len(input10[0])
+}
+
+func solve10a() int {
+	total := 0
+	for y := range rows {
+		for x := range cols {
+			if input10[y][x] == 0 {
+				visited := make(map[int]interface{}, 0)
+				paths10a(y, x, visited)
+				total += len(visited)
+			}
+		}
+	}
+	return total
+}
+
+func paths10a(y, x int, visited map[int]interface{}) {
+	if input10[y][x] == 9 {
+		visited[y*cols+x] = true
+		return
+	}
+	if y-1 >= 0 && input10[y-1][x] == input10[y][x]+1 {
+		paths10a(y-1, x, visited)
+	}
+	if y+1 < rows && input10[y+1][x] == input10[y][x]+1 {
+		paths10a(y+1, x, visited)
+	}
+	if x-1 >= 0 && input10[y][x-1] == input10[y][x]+1 {
+		paths10a(y, x-1, visited)
+	}
+	if x+1 < cols && input10[y][x+1] == input10[y][x]+1 {
+		paths10a(y, x+1, visited)
+	}
+}
+
+func solve10b() int {
+	total := 0
+	for y := range rows {
+		for x := range cols {
+			if input10[y][x] == 0 {
+				total += paths10b(y, x, 0)
+			}
+		}
+	}
+	return total
+}
+
+func paths10b(y, x, acc int) int {
+	if input10[y][x] == 9 {
+		return acc + 1
+	}
+	total := 0
+	if y-1 >= 0 && input10[y-1][x] == input10[y][x]+1 {
+		total += paths10b(y-1, x, acc)
+	}
+	if y+1 < rows && input10[y+1][x] == input10[y][x]+1 {
+		total += paths10b(y+1, x, acc)
+	}
+	if x-1 >= 0 && input10[y][x-1] == input10[y][x]+1 {
+		total += paths10b(y, x-1, acc)
+	}
+	if x+1 < cols && input10[y][x+1] == input10[y][x]+1 {
+		total += paths10b(y, x+1, acc)
+	}
+	return total
+}
