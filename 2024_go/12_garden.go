18daee203637dedc3f5cbbb08ee774d9e3193a91
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-12-28 19:30:19 +0100

    2024 12 b

--- 2024_go/12_garden.go
+++ 2024_go/12_garden.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-// 1550156 XX (sample 1930 XX)
+// 1550156 946084 (sample 1930 1206)
 func main() {
-	solve("12_sample_2.txt", line12, nil, func() int { return solve12(false) }, func() int { return solve12(true) })
+	solve("12_input.txt", line12, nil, func() int { return solve12(false) }, func() int { return solve12(true) })
 }
 
 var (
@@ -38,9 +32,10 @@ func solve12(sides bool) int {
 }
 
 func calc12(yini, xini int, visited map[int]bool, sides bool) int {
-	a, p := 0, 0
+	a := 0
 	ch := input12[yini][xini]
 	list := []int{yini*cols12 + xini}
+	borders := make(map[int]interface{})
 	for len(list) > 0 {
 		pos := list[len(list)-1]
 		y, x := pos/cols12, pos%cols12
@@ -51,16 +46,16 @@ func calc12(yini, xini int, visited map[int]bool, sides bool) int {
 		visited[pos] = true
 		a++
 		if y == 0 || input12[y-1][x] != ch {
-			p++
+			borders[0*cols12*rows12+y*cols12+x] = nil
 		}
 		if y == rows12-1 || input12[y+1][x] != ch {
-			p++
+			borders[1*cols12*rows12+y*cols12+x] = nil
 		}
 		if x == 0 || input12[y][x-1] != ch {
-			p++
+			borders[2*cols12*rows12+y*cols12+x] = nil
 		}
 		if x == cols12-1 || input12[y][x+1] != ch {
-			p++
+			borders[3*cols12*rows12+y*cols12+x] = nil
 		}
 		if y > 0 {
 			list = append(list, pos-cols12)
@@ -75,48 +70,47 @@ func calc12(yini, xini int, visited map[int]bool, sides bool) int {
 			list = append(list, pos+1)
 		}
 	}
-	return a * p
-}
-
-// DELETE
-
-func solve(inputFile string, processLine1, processLine2 func(string), ret1, ret2 func() int) {
-	f1, _ := os.Open("inputs/" + inputFile)
-	defer f1.Close()
-	scanner1 := bufio.NewScanner(f1)
-	for scanner1.Scan() {
-		line := scanner1.Text()
-		if processLine1 != nil {
-			processLine1(line)
+	if sides {
+		for yini := range rows12 {
+			for xini := range cols12 {
+				if _, found := borders[0*cols12*rows12+yini*cols12+xini]; found {
+					for x := xini + 1; x < cols12; x++ {
+						if _, found := borders[0*cols12*rows12+yini*cols12+x]; found {
+							delete(borders, 0*cols12*rows12+yini*cols12+x)
+						} else {
+							break
+						}
+					}
+				}
+				if _, found := borders[1*cols12*rows12+yini*cols12+xini]; found {
+					for x := xini + 1; x < cols12; x++ {
+						if _, found := borders[1*cols12*rows12+yini*cols12+x]; found {
+							delete(borders, 1*cols12*rows12+yini*cols12+x)
+						} else {
+							break
+						}
+					}
+				}
+				if _, found := borders[2*cols12*rows12+yini*cols12+xini]; found {
+					for y := yini + 1; y < rows12; y++ {
+						if _, found := borders[2*cols12*rows12+y*cols12+xini]; found {
+							delete(borders, 2*cols12*rows12+y*cols12+xini)
+						} else {
+							break
+						}
+					}
+				}
+				if _, found := borders[3*cols12*rows12+yini*cols12+xini]; found {
+					for y := yini + 1; y < rows12; y++ {
+						if _, found := borders[3*cols12*rows12+y*cols12+xini]; found {
+							delete(borders, 3*cols12*rows12+y*cols12+xini)
+						} else {
+							break
+						}
+					}
+				}
+			}
 		}
 	}
-	fmt.Println(ret1())
-
-	f2, _ := os.Open("inputs/" + inputFile)
-	defer f2.Close()
-	scanner2 := bufio.NewScanner(f2)
-	for scanner2.Scan() {
-		line := scanner2.Text()
-		if processLine2 != nil {
-			processLine2(line)
-		}
-	}
-	fmt.Println(ret2())
-}
-
-func abs[T int](x T) T {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func sign[T int](x T) T {
-	if x == 0 {
-		return 0
-	}
-	if x < 0 {
-		return -1
-	}
-	return 1
+	return a * len(borders)
 }
