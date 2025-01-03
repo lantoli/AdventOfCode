2391303623f285b4ebc3b7632f7e5bce29cc68c6
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-01-03 22:47:13 +0100

    2024 17 b wip

--- 2024_go/17_computer.go
+++ 2024_go/17_computer.go
@@ -9,24 +9,44 @@ var (
 	isInput17 = true
 )
 
-// 3,6,3,7,0,7,0,3,0 XX (sample 4,6,3,5,6,3,5,2,1,0 XX)
-// takes minutes using parallelism, algorithm can surely be optimized to run in seconds in serial
+// 3,6,3,7,0,7,0,3,0 869230000000 too low (sample 4,6,3,5,6,3,5,2,1,0 117440)
+// takes hours, algorithm can surely be optimized
 func main() {
-	solve17(false)
+	//solve17a()
+	solve17b()
 }
 
-func solve17(partB bool) {
+func solve17a() {
 	a, b, c := 729, 0, 0
 	program := []int{0, 1, 5, 4, 3, 0}
-	out := make([]int, 0)
 	if isInput17 {
 		a = 51064159
 		program = []int{2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 6, 5, 5, 3, 0}
 	}
-	// examples
-	// a, b, c, program = 0, 0, 9, []int{2, 6}
-	// a, b, c, program = 10, 0, 0, []int{5, 0, 5, 1, 5, 4}
-	// a, b, c, program = 2024, 0, 0, []int{0, 1, 5, 4, 3, 0}
+	out := calc17(a, b, c, program, false)
+	fmt.Println(strings.Trim(strings.Join(strings.Fields(strings.Trim(fmt.Sprint(out), "[]")), ","), " "))
+}
+
+func solve17b() {
+	b, c := 0, 0
+	program := []int{0, 3, 5, 4, 3, 0}
+	if isInput17 {
+		program = []int{2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 6, 5, 5, 3, 0}
+	}
+	for a := 0; ; a++ {
+		if a%10_000_000 == 0 {
+			fmt.Println(a)
+		}
+		if calc17(a, b, c, program, true) != nil {
+			fmt.Println(a)
+			break
+		}
+	}
+}
+
+func calc17(a, b, c int, program []int, checkEqual bool) []int {
+	out := make([]int, 0)
+	outCount := 0
 	for ip := 0; ip < len(program); {
 		opcode, literal := program[ip], program[ip+1]
 		combo := literal
@@ -53,7 +73,15 @@ func solve17(partB bool) {
 		case 4: // bxc
 			b ^= c
 		case 5: // out
-			out = append(out, combo%8)
+			val := combo % 8
+			if checkEqual {
+				if outCount == len(program) || val != program[outCount] {
+					return nil
+				}
+				outCount++
+			} else {
+				out = append(out, val)
+			}
 		case 6: // bdv
 			b = a / (1 << combo)
 		case 7: // cdv
@@ -65,5 +93,12 @@ func solve17(partB bool) {
 			ip += 2
 		}
 	}
-	fmt.Println(strings.Trim(strings.Join(strings.Fields(strings.Trim(fmt.Sprint(out), "[]")), ","), " "))
+	if checkEqual {
+		if outCount != len(program) {
+			return nil
+		} else {
+			return program
+		}
+	}
+	return out
 }
