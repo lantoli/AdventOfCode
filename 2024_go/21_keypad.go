0e8c0f22c33e125675f26cb84a83e2ceceefaf99
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-01-06 12:35:38 +0100

    2024 21 a wip

--- 2024_go/21_keypad.go
+++ 2024_go/21_keypad.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"slices"
+	"strings"
 )
 
 // XX XX (sample 126384 XX)
@@ -13,37 +16,112 @@ func main() {
 
 var (
 	input21 = make([]string, 0)
+	cols21  = 3
+	dirs21  = [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
 )
 
 func line21(line string) {
 	input21 = append(input21, line)
+
 }
 
 func solve21(b bool) int {
 	total := 0
-	for codes := range input21 {	
-		total +=calc01a()	
+	for _, codes := range input21 {
+		total += calc21(codes)
 	}
 	return total
 }
 
-func calc21(codes string) int
-	const n = 10
-	dirpad := [][]string{
-		{"7", "8", "9"},
-		{"4", "5", "6"},
-		{"1", "2", "3"},
-		{"", "0", "A"},
-	}
-	numpad := [][]string{
-		{"", "^", "A"},
-		{"<", "v", ">"},
-	}
-	dirpos := 3*n + 2
-	num1pos := 2
-	for _, code := range codes {
-	}
+func calc21(codes string) int {
+	numpad := []rune{'7', '8', '9', '4', '5', '6', '1', '2', '3', 0, '0', 'A'}
+	dirpad := []rune{0, '^', 'A', '<', 'v', '>'}
+
+	nummap := path21(numpad)
+	dirmap := path21(dirpad)
+	
+		num := 'A'
+		dir1 := 'A'
+		minlen = math.MaxInt
+		for _, code := range codes {
+			for _, numinfo := nummap[num][code] {
+				paths, dirs := numinfo.path, numinfo.dirs
+				for i := range paths {
+					
+				}
+			}
+		}
+	
+	_, _ = nummap, dirmap
 	return 0
+}
+
+type st21 struct {
+	pos  int
+	path string
+}
+
+type stp21 struct {
+	path string
+	dirs string
+}
+
+func path21(grid []rune) map[rune]map[rune][]stp21 {
+	rets := make(map[rune]map[rune][]stp21)
+	rows := len(grid) / cols21
+	for pos, button := range grid {
+		dist := make(map[rune][]string)
+		list := []st21{{pos, ""}}
+		for len(list) > 0 {
+			cur := list[0]
+			list = list[1:]
+			paths, _ := dist[grid[cur.pos]]
+			if slices.Contains(paths, cur.path) {
+				continue
+			}
+			dist[grid[cur.pos]] = append(paths, cur.path)
+			x, y := cur.pos/cols21, cur.pos%cols21
+			for _, dir := range dirs21 {
+				xnew, ynew := x+dir[0], y+dir[1]
+				posnew := xnew*cols21 + ynew
+				if xnew >= 0 && xnew < rows && ynew >= 0 && ynew < cols21 && grid[posnew] != 0 && !strings.ContainsRune(cur.path, grid[posnew]) {
+					list = append(list, st21{xnew*cols21 + ynew, cur.path + string(grid[posnew])})
+				}
+			}
+		}
+		ret := make(map[rune][]stp21)
+		rets[button] = ret
+		for k, v := range dist {
+			minlen := math.MaxInt
+			for _, path := range v {
+				minlen = min(minlen, len(path))
+			}
+			for _, path := range v {
+				if len(path) == minlen {
+					dirs := ""
+					rold := grid[pos]
+					for _, rnew := range path {
+						posold := slices.Index(grid, rold)
+						posnew := slices.Index(grid, rnew)
+						if posnew == posold+1 {
+							dirs += ">"
+						} else if posnew == posold-1 {
+							dirs += "<"
+						} else if posnew == posold+cols21 {
+							dirs += "v"
+						} else if posnew == posold-cols21 {
+							dirs += "^"
+						} else {
+							panic("bad path")
+						}
+						rold = rnew
+					}
+					ret[k] = append(ret[k], stp21{path, dirs})
+				}
+			}
+		}
+	}
+	return rets
 }
 
 // DELETE
